d459f53bf3ebfb837dc7ceee32349e37daa03b81
hyobim <66728397+hyobim@users.noreply.github.com>
2020-10-08 01:05:38 +0900

    백준1935번[201008]-stack으로 구현

--- 1935번.cpp
+++ 1935번.cpp
@@ -1,166 +1,114 @@
-#include<iostream>
+#include <iostream>
 #include<string>
-#include <ctype.h>
 using namespace std;
 
-class BTNode {
+class Node {
 public:
-	int data;
-	BTNode* left;
-	BTNode* right;
-
-	BTNode* MakeBTreeNode() {
-		BTNode* newnode = new BTNode;
-		newnode->left = NULL;
-		newnode->right = NULL;
-
-		return newnode;
-	}
-
-	int getData(BTNode* bt) {
-		return bt->data;
-	}
-	void setData(BTNode* bt, int d) {
-		bt->data = d;
-	}
-	BTNode* getLeft(BTNode* bt) {
-		return bt->left;
-	}
-	BTNode* getRight(BTNode* bt) {
-		return bt->right;
-	}
-	void setLeft(BTNode* main, BTNode* sub) {
-		if (main->left != NULL)
-			delete(main->left);
-		else
-			main->left = sub;
-	}
-	void setRight(BTNode* main, BTNode* sub) {
-		if (main->right != NULL)
-			delete(main->right);
-		else
-			main->right = sub;
-	}
-};
-typedef BTNode* Data;
-
-class Node
-{
-public:
-	Data data; //btnode의 주소값을 저장함 
+	double data;
 	Node* next;
+
 	Node() {
 		data = 0;
 		next = NULL;
 	}
-	
 };
-class stack {
+
+class Struct {
 public:
-	Node * tail;
-	Node* head;
+	Node * head;
+	Node* tail;
 	int size;
-	stack() {
-		size = 0;
-		tail = NULL;
+
+	Struct() {
 		head = NULL;
+		tail = NULL;
+		size = 0;
 	}
-	Data peek(stack* st) {
+	double ShowTop() {
 		if (size == 0)
-			return NULL;
+			return 0;
 		else
-		{
-			return st->tail->data;
+			return tail->data;
+	}
+	int Size() { return size; }
+	void push(double c) {
+		Node* newnode = new Node;
+		newnode->data = c;
+		newnode->next = NULL;
+		if (head == NULL) {
+			head = newnode;
+			tail = newnode;
 		}
-	}
-	Data pop(stack* st) {
-		if (size == 0)
-			return NULL;
 		else
 		{
+			newnode->next = tail;
+			tail = newnode;
+		}
+		size++;
+	}
+	double pop() {
+		if (size == 0)
+			return 0;
+		else {
 			Node* tmp = new Node;
 			tmp = tail;
-			Data tmmp = tmp->data;
+			double tmpp = tmp->data;
 			tail = tail->next;
 
 			delete tmp;
-			return tmmp;
+			size--;
+			return tmpp;
 		}
-		size--;
 	}
-	void push(stack* st , Data x) {
-		Node* newnode = new Node;
-		newnode->data = x;
-		newnode->next = NULL;
-		if (st->head == NULL)
-		{
-			st->head = newnode;
-			st->tail = newnode;
-		}
-		else {
-			newnode->next = st->tail;
-			st->tail = newnode;
-		}
-		size++;
-	
-	}
-	
 };
-Data MakeExpTree(string exp, int a[]) {
-	stack s;
 
-	BTNode* pn=NULL;
-
-
-	int exl = exp.length();
-	for (int i = 0; i < exl; i++) {
-
-		pn= pn->MakeBTreeNode();
-
-		if (int(exp[i])>=65) {
-			pn->setData(pn, a[int(exp[i])-65]);
-		}
-		else {
-			pn->setRight(pn, s.pop(&s));
-			pn->setLeft(pn, s.pop(&s));
-			pn->setData(pn, exp[i]);
-		}
-
-		s.push(&s , pn);
-	}
-	return s.pop(&s);
-}
-double EvaluateExpTree(BTNode* bt) {
-	double op1, op2;
-	if (bt->getLeft(bt) == NULL && bt->getRight(bt) == NULL)
-		return bt->getData(bt);
-
-	op1 = EvaluateExpTree(bt->getLeft(bt));
-	op2 = EvaluateExpTree(bt->getRight(bt));
-	switch (bt->getData(bt))
-	{
-	case'+':
-		return op1 + op2;
-	case'-':
-		return op1 - op2;
-	case'*':
-		return op1 * op2;
-	case'/':
-		return op1 / op2;
-	}
-}
 int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	int n, a[26];
+	Struct s;
+	int n;
+	double arr[26];
+	double a, b;
 	cin >> n;
-	string exp;
-	cin >> exp;
+	string command;
+	cin >> command;
+
+	int size = command.length();
+	
+	
 	for (int i = 0; i < n; i++) {
-		cin >> a[i];
+		cin >> arr[i];
 	}
-	BTNode* etree = MakeExpTree(exp,a);
+
+	for (int i = 0; i < size; i++) {
+		if (int(command[i]) >= 65) {
+			s.push(arr[int(command[i]) - 65]);
+		}
+		else if (command[i] == '*')
+		{
+			a = s.pop();
+			b = s.pop();
+			s.push(a*b);
+		}
+		else if (command[i] == '/')
+		{
+			a = s.pop();
+			b = s.pop();
+			s.push(b/a);
+		}
+		else if (command[i] == '-')
+		{
+			a = s.pop();
+			b = s.pop();
+			s.push(b - a);
+		}
+		else if (command[i] == '+')
+		{
+			a = s.pop();
+			b = s.pop();
+			s.push(b + a);
+		}
+	}
 	cout << fixed;
 	cout.precision(2);
-	cout<<EvaluateExpTree(etree)<<"\n";
+	cout << s.pop() << "\n";
 }
+
